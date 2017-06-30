3b03905f1e086e86c0ffe4870045c84e7f29610e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-06-30 12:45:10 +0000

    Deploy inilabs/libcaer to github.com/inilabs/libcaer.git:gh-pages

---
+++ search/variables_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['finevalue',['fineValue',['../structcaer__bias__coarsefine.html#adb79ab2e6169db9aa23dd2ebced923fd',1,'caer_bias_coarsefine::fineValue()'],['../structcaer__bias__dynapse.html#a69248792591a29ff09d84f1084794801',1,'caer_bias_dynapse::fineValue()']]]
+];
